7e192b7105bfbad763ec651a9f02373ad432e036
VBrawl <konstantosjim@gmail.com>
2024-07-14 20:00:13 +0300

    Updated screen to return a tqdm progress bar for processing

--- src/screen.py
+++ src/screen.py
@@ -1,13 +1,18 @@
 from colorama import Fore
+from tqdm import tqdm
 
 
 class Screen:
+    PROGRESS_MAX = 10
+
     def __init__(self, error_color: str = Fore.RED,
                  warning_color: str = Fore.YELLOW,
-                 success_color: str = Fore.GREEN):
+                 success_color: str = Fore.GREEN,
+                 processing_color: str = Fore.BLUE):
         self.error_color = error_color
         self.warning_color = warning_color
         self.success_color = success_color
+        self.processing_color = processing_color
 
     @staticmethod
     def log(*args, **kwargs):
@@ -22,6 +27,5 @@ class Screen:
     def success(self, *args, **kwargs):
         print(self.success_color, '[+]', *args, **kwargs)
 
-    @staticmethod
-    def processing(*args, **kwargs):
-        print(Fore.RESET, '[*]', *args, **kwargs)
+    def processing(self, steps: int = PROGRESS_MAX, description: str = ""):
+        return tqdm(range(steps), desc=self.processing_color + description)
