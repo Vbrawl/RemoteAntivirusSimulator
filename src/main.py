bf2481c89b1b7b9b88bcf5ceace5440d37d23f56
VBrawl <konstantosjim@gmail.com>
2024-07-14 20:00:51 +0300

    Testing screen

--- src/main.py
+++ src/main.py
@@ -1,4 +1,13 @@
+from screen import Screen
+import time
+
+import random
 
 
 if __name__ == "__main__":
-    pass
+    screen = Screen()
+    handle = screen.processing(description="Hello World")
+    for i in handle:
+        for _ in range(random.randint(1, 10)):
+            handle.update(0)
+            time.sleep(0.5)
