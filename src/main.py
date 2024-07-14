f8e5f342511aa9f3653c0f480cc7adeb1386e961
VBrawl <konstantosjim@gmail.com>
2024-07-15 01:23:39 +0300

    Added MalwareScan scene in the plot

--- src/main.py
+++ src/main.py
@@ -1,11 +1,14 @@
 from screen import Screen
 from plot import Plot
 from scenes.connection import Connection
+from scenes.malware_scan import MalwareScan
 
 
 if __name__ == "__main__":
     screen = Screen()
+
     plot = Plot([
-        Connection()
+        Connection(),
+        MalwareScan()
     ])
     plot.runAll(screen)
