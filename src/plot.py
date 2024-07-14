2588740ac59f7eb4adaf8c17a6cb11d96cad2375
VBrawl <konstantosjim@gmail.com>
2024-07-14 20:01:21 +0300

    Added screen as a required parameter for scenes

--- src/plot.py
+++ src/plot.py
@@ -1,11 +1,12 @@
 from typing import Optional
 from scene import Scene
+from screen import Screen
 
 
 class Plot:
     def __init__(self, scenes: Optional[list[Scene]] = None):
         self.scenes = scenes if scenes is not None else []
 
-    def runAll(self):
+    def runAll(self, screen: Screen):
         for scene in self.scenes:
-            scene.run()
+            scene.run(screen)
--- src/scene.py
+++ src/scene.py
@@ -1,5 +1,6 @@
+from screen import Screen
 
 
 class Scene:
-    def run(self):
+    def run(self, screen: Screen):
         raise NotImplementedError(f"{self.__class__.__qualname__}.run")
