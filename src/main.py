ca3740a20f49fe421afbac317931efd0b6d04d14
VBrawl <konstantosjim@gmail.com>
2024-07-18 11:02:19 +0300

    Added different scan modes

--- src/main.py
+++ src/main.py
@@ -1,3 +1,4 @@
+from typing import Any
 from screen import Screen
 from plot import Plot
 from scenes.connection import Connection
@@ -7,8 +8,21 @@ from scenes.malware_scan import MalwareScan
 if __name__ == "__main__":
     screen = Screen()
 
+    # The dictionary holds parameters for the MalwareScan
+    scan_modes:list[tuple[str, dict[str, Any]]] = [
+        ("Quick Scan", {}),
+        ("Full Scan", {
+            "execution_time": 8 * 60,
+            "scan_multiplier": 0.05,
+            "maximum_scan_duration": 10,
+            "minimum_scan_duration": 0.05
+        })
+    ]
+    mode_index = screen.menu("Remote Antivirus", list(map(lambda x: x[0], scan_modes)))
+    mode: tuple[str, dict[str, Any], dict[str, Any]] = scan_modes[mode_index]
+
     plot = Plot([
         Connection(),
-        MalwareScan()
+        MalwareScan(**mode[1])
     ])
     plot.runAll(screen)
